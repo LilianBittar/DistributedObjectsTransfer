ace6974ceaac817e616c4761b4e1b20ef65d43f6
Michael Bui <michaelbui99@hotmail.com>
2021-10-01 10:07:14 +0200

    Csharp project

---
+++ csharp/SDJ3DistributedObjectsTransfer/SDJ3DistributedObjectsTransfer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDJ3DistributedObjectsTransfer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
